3a1812fc9466dfe67fd0612fd321c065476c8791
nikulinkv <nikulinkv@yandex.ru>
2021-08-09 22:51:43 +0500

    bags fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,153 +1,164 @@
-    $('a[href^="#"], *[data-href^="#"]').on("click", (function (e) {
-        e.preventDefault();
-        var c = $(this).attr("data-href") ? $(this).attr("data-href") : $(this).attr("href");
-        $("html,body").stop().animate({
-            scrollTop: $(c).offset().top
-        }, 1e3)
-    })),
+// Smooth scroll
+$('a[href^="#"], *[data-href^="#"]').on("click", (function (e) {
+    e.preventDefault();
+    var c = $(this).attr("data-href") ? $(this).attr("data-href") : $(this).attr("href");
+    $("html,body").stop().animate({
+        scrollTop: $(c).offset().top
+    }, 1e3)
+})),
 
-    $(window).on("load", (function () {
-        $("span[id^=icon]").addClass("icon_fly");
-        setTimeout(function () {
-            $(".header__icon, .price__icon").css('transition', '0s');
-        }, 1000);
-    })),
+// Fly icon
+$(window).on("load", (function () {
+    $("span[id^=icon]").addClass("icon_fly");
+    setTimeout(function () {
+        $(".header__icon, .price__icon").css({
+            'opacity' : '1',
+            'transition': 'all 1s'
+        });
+    }, 300);
+})),
 
-    $('input[type="tel"]').inputmask({
-        mask: "+7 (999) 999-99-99"
-    }),
+// Phone mask
+$('input[type="tel"]').inputmask({
+    mask: "+7 (999) 999-99-99"
+}),
 
-    $(document).on('mouseup', function (e){
-        var button = $(".button-mobile")
-		var div = $(".header-nav__list");
+// Mobile menu
+$(document).on('mouseup', function (e){
+    var button = $(".button-mobile")
+    var div = $(".header-nav__list");
 
-		if (!div.is(e.target)
-            && window.innerWidth < 992
-		    && div.is(":visible")) {
-			div.hide("slow");
-		}
-        else if (button.is(e.target)
-            && div.is(":hidden")) {
-            div.show("slow");
+    if (!div.is(e.target)
+        && window.innerWidth < 992
+        && div.is(":visible")) {
+        div.hide("slow");
+    }
+    else if (button.is(e.target)
+        && div.is(":hidden")) {
+        div.show("slow");
+    }
+});
+
+// Owl-carousel
+$(".owl-carousel").owlCarousel({
+    loop: !0,
+    margin: 50,
+    autoplay: !0,
+    smartSpeed: 2e3,
+    autoplayTimeout: 6e3,
+    autoplayHoverPause: !0,
+    responsive: {
+        0: {
+            items: 1,
+            dots: !0
+        },
+        700: {
+            items: 2,
+            dots: !0
+        },
+        1024: {
+            items: 3
         }
-	});
+    }
+});
+var owl = $(".owl-carousel");
+owl.owlCarousel(),
+$(".wrap__arrow_right").on("click", (function () {
+    owl.trigger("next.owl.carousel")
+})),
+$(".wrap__arrow_left").on("click", (function () {
+    owl.trigger("prev.owl.carousel", [300])
+}));
 
-    $(".owl-carousel").owlCarousel({
-        loop: !0,
-        margin: 50,
-        autoplay: !0,
-        smartSpeed: 2e3,
-        autoplayTimeout: 6e3,
-        autoplayHoverPause: !0,
-        responsive: {
-            0: {
-                items: 1,
-                dots: !0
-            },
-            700: {
-                items: 2,
-                dots: !0
-            },
-            1024: {
-                items: 3
-            }
-        }
-    });
-    var owl = $(".owl-carousel");
-    owl.owlCarousel(),
-    $(".wrap__arrow_right").on("click", (function () {
-        owl.trigger("next.owl.carousel")
-    })),
-    $(".wrap__arrow_left").on("click", (function () {
-        owl.trigger("prev.owl.carousel", [300])
-    }));
-
-    const scroll = function (e) {
-        e.preventDefault()
-    },
-    disableScroll = function (e) {
-        window.addEventListener("mousewheel", scroll, {
-                passive: !1
-            }),
-            $("body, html").on("DOMMouseScroll", (function (e) {
+// Onn/off work scroll
+const scroll = function (e) {
+    e.preventDefault()
+},
+disableScroll = function (e) {
+    window.addEventListener("mousewheel", scroll, {
+            passive: !1
+        }),
+        $("body, html").on("DOMMouseScroll", (function (e) {
+            return !1
+        })),
+        $("body, html").on("keydown", (function (e) {
+            if (e.which > 32 && e.which < 41)
                 return !1
-            })),
-            $("body, html").on("keydown", (function (e) {
-                if (e.which > 32 && e.which < 41)
-                    return !1
-            }))
-    },
-    enableScroll = function (e) {
-        window.removeEventListener("mousewheel", scroll, {
-                passive: !1
-            }),
-            $("body, html").off("keydown DOMMouseScroll")
-    };
-
-
-    $("form").each((function () {
-        $(this).validate({
-            errorPlacement: (e, c) => !0,
-            focusInvalid: !1,
-            rules: {
-                "Имя": {
-                    required: !0
-                },
-                "Телефон": {
-                    required: !0,
-                    minlenght: 11
-                },
-                "Текст": {
-                    required: !0
-                }
-            },
-            submitHandler(e) {
-                let c = $(e);
-                let modal = $('.modal');
-                let thankYou = $('.thank-you');
-                return $.ajax({
-                        type: "POST",
-                        url: "mail.php",
-                        data: c.serialize(),
-                        success: function (e) {
-                            c.trigger("reset"),
-                            c.hide(300),
-                            thankYou.show(300)
-                        }
-                    }),
-                    !1
-            }
-        })
-    })),
-
-    $(window).on("resize load", (function (e) {
-    var e = $(".head__wrap");
-    $(window).width() < 768 ? (e.remove(),
-            e.appendTo(".mobile__head")) : (e.remove(),
-            e.appendTo(".head")),
-        $(".header__button, .head__button, .about__button, .features__button, .footer__button").on("click", (function () {
-            $(".modal").css("display", "flex"),
-                $(".modal").fadeIn(1500, disableScroll)
         }))
+},
+enableScroll = function (e) {
+    window.removeEventListener("mousewheel", scroll, {
+            passive: !1
+        }),
+        $("body, html").off("keydown DOMMouseScroll")
+};
 
-        $("#text").on("mouseenter", function(){
-            if($(this)[0].scrollHeight > $(this)[0].clientHeight) {
-                $("body, html", enableScroll)
+// Form validation
+$("form").each((function () {
+    $(this).validate({
+        errorPlacement: (e, c) => !0,
+        focusInvalid: !1,
+        rules: {
+            "Имя": {
+                required: !0
+            },
+            "Телефон": {
+                required: !0,
+                minlenght: 11
+            },
+            "Текст": {
+                required: !0
             }
-            else {
-                $("body, html", disableScroll)
-            }
-        })
+        },
+        submitHandler(e) {
+            let c = $(e);
+            let modal = $('.modal');
+            let thankYou = $('.thank-you');
+            return $.ajax({
+                    type: "POST",
+                    url: "mail.php",
+                    data: c.serialize(),
+                    success: function (e) {
+                        c.trigger("reset"),
+                        c.hide(300),
+                        thankYou.show(300)
+                    }
+                }),
+                !1
+        }
+    })
+})),
 
-    $(".modal").on("click", (function (e) {
-        e.target == this && ($(this).fadeOut(1500, enableScroll),
-            $("form :input").val(""),
-            $(".form__input, .form__text").removeClass('error'))
+// Disabling scrolling when opening a modal window
+$(window).on("resize load", (function (e) {
+var e = $(".head__wrap");
+$(window).width() < 768 ? (e.remove(),
+        e.appendTo(".mobile__head")) : (e.remove(),
+        e.appendTo(".head")),
+    $(".header__button, .head__button, .about__button, .features__button, .footer__button").on("click", (function () {
+        $(".modal").css("display", "flex"),
+            $(".modal").fadeIn(1500, disableScroll)
     }))
 
-    $(".modal__close").on("click", (function (e) {
-        e.target == this && ($(".modal").fadeOut(1500, enableScroll),
-            $("form :input").val(""),
-            $(".form__input, .form__text").removeClass('error'))
-    }))
+    $("#text").on("mouseenter", function(){
+        if($(this)[0].scrollHeight > $(this)[0].clientHeight) {
+            $("body, html", enableScroll)
+        }
+        else {
+            $("body, html", disableScroll)
+        }
+    })
+
+// Clearing the form after sending the data
+$(".modal").on("click", (function (e) {
+    e.target == this && ($(this).fadeOut(1500, enableScroll),
+        $("form :input").val(""),
+        $(".form__input, .form__text").removeClass('error'))
+}))
+
+$(".modal__close").on("click", (function (e) {
+    e.target == this && ($(".modal").fadeOut(1500, enableScroll),
+        $("form :input").val(""),
+        $(".form__input, .form__text").removeClass('error'))
+}))
 }));
